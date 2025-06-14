3dad6b56437de42a1e77fc2cc0e6fd74c066a3f1
ParkGyumin <gyumin1129@kookmin.ac.kr>
2025-06-14 17:37:37 +0900

    12

--- train_vmamba_fscil.sh
+++ train_vmamba_fscil.sh
@@ -1,8 +1,4 @@
 GPUS=1
 work_dir=work_dirs/vmamba_base_mambafscil_cub
-#bash tools/dist_train.sh configs/cub/vmamba_base_etf_bs512_80e_cub_mambafscil.py $GPUS --work-dir ${work_dir} --seed 0 --deterministic
-<<<<<<< HEAD
+bash tools/dist_train.sh configs/cub/vmamba_base_etf_bs512_80e_cub_mambafscil.py $GPUS --work-dir ${work_dir} --seed 0 --deterministic
 bash tools/run_fscil.sh configs/cub/vmamba_base_etf_bs512_80e_cub_eval_mambafscil.py ${work_dir} ${work_dir}/best.pth $GPUS --seed 0 --deterministic
-=======
-bash tools/run_fscil.sh configs/cub/vmamba_base_etf_bs512_80e_cub_eval_mambafscil.py ${work_dir} ${work_dir}/best.pth $GPUS --seed 0 --deterministic
->>>>>>> fef749cf3f9660aa91d6924f457a54383b720d29
